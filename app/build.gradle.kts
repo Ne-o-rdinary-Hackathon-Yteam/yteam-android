cf1f9d34ae1f93ed31b82360a6336363650d7190
junsuPark <krpjs0508@gmail.com>
2024-06-01 16:02:17 +0900

    dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,9 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.coil.compose)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.gson)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
